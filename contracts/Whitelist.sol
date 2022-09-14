b85faaf4e3354b12fb74acf57783e8b9f3eb4d75
royvardhan <royvardhan.official@gmail.com>
2022-09-14 21:05:42 +0530

    solidity implementation done

---
+++ contracts/Whitelist.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+
+contract Whitelist {
+    bytes32 public merkleRoot;
+
+    constructor(bytes32 _merkleRoot) {
+        merkleRoot = _merkleRoot;
+    }
+
+    function verify(bytes32[] calldata _proof, uint64 _maxAllowanceToMint) pubic view returns(bool) {
+        bytes32 leaf = keccak256(abi.encode(msg.sender, _maxAllowanceToMint));
+        bool verified = MerkelProof.verify(_proof, merkleRoot, leaf);
+        return verified;
+    }
+
+}
